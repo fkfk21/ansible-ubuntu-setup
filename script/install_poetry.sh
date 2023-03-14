1bf950d0041716524ff4aefce60cb5063c81336d
Kenji Fukuda <fk.kenji.21@gmail.com>
2023-03-14 18:20:23 +0900

    fix install poetry.sh

--- script/install_poetry.sh
+++ script/install_poetry.sh
@@ -1,5 +1,5 @@
 # install pyenv
-# curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 # install poetry
 curl -sSL https://install.python-poetry.org | python3 -
