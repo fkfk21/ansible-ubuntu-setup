e41e2a0600343fed0b9204280c27d3251d6f9e40
Kenji Fukuda <fk.kenji.21@gmail.com>
2024-10-15 15:53:53 +0900

    update poetry installation

--- script/install_poetry.sh
+++ script/install_poetry.sh
@@ -3,7 +3,13 @@ curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-
 
 # install poetry
 # curl -sSL https://install.python-poetry.org | python3 -
-sudo apt install python3-poetry
+# sudo apt install python3-poetry
+
+## install poetry from pipx
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+sudo apt install python3-venv
+pipx install poetry
 
 # settings
 poetry config virtualenvs.in-project true
